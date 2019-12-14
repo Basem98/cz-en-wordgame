822415d53a24d5fe0992fd7133b3e0cc4a901a4e
Basem <basem.mostafa98@hotmail.com>
2019-12-14 11:20:22 -0800

    another deployment trial

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,23 @@ const passport = require('passport');
 const { establishAuthStrategy } = require('./api/authentication/jwtStrategy');
 const config = require('./config/envConfig');
 const connectToDatabase = require('./config/connectToDb');
+const path = require('path');
 
 const strategy = establishAuthStrategy().generate();
 const server = express();
 
+server.use(express.static(path.join(__dirname, '../dist/czWordGame')));
 server.use(cors({ origin: '*' }));
 passport.use(strategy);
 server.use(passport.initialize());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use('/', require('./api/routes'));
+
+server.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/czWordGame/index.html'));
+})
+
 
 connectToDatabase(config.DB.URL);
 server.listen(config.APP.PORT, () => {
